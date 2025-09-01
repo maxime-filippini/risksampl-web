fc5c2c68aa749ac9644f6594d42ddb96aebaf44f
Maxime Filippini <maxime.filippini@gmail.com>
2025-09-01 14:27:56 +0200

    add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'maxime-filippini',
+				project: 'javascript-sveltekit'
+			}
+		}),
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson()
+	],
 	server: {
 		allowedHosts: ['mmbp.follow-scylla.ts.net']
 	}
