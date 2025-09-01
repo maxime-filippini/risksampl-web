d26ec92e34825dbd5e670f000445355d18d9e32d
Maxime Filippini <maxime.filippini@gmail.com>
2025-09-01 11:11:17 +0200

    add posthog

---
+++ src/routes/+layout.js
@@ -0,0 +1,14 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init('phc_sGqtRHbSH8yHBsY9SsexmXymJ9zoKu7QSWuL4uKenrE', {
+			api_host: 'https://eu.i.posthog.com',
+			defaults: '2025-05-24',
+			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+		});
+	}
+
+	return;
+};
