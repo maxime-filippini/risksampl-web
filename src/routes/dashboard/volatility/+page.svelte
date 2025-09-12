41889653df4a665a7fe7564e8f72410b820307b3
Maxime Filippini <maxime.filippini@gmail.com>
2025-09-12 19:49:01 +0200

    tab format for dashboard

---
+++ src/routes/dashboard/volatility/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import ExAnteVolChart from '../ExAnteVolChart.svelte';
+
+	let { data } = $props();
+	const selectedPortfolioIds: () => string[] = getContext('selectedPortfolioIds');
+</script>
+
+<ExAnteVolChart
+	portfolios={data.ptfs}
+	dayMeasures={data.dayMeasures}
+	selectedPortfolioIds={selectedPortfolioIds()}
+/>
